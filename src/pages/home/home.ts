34b1adaf7f2f5b78b07c99976ec1709cece21cef
Carol Braileanu <carol.braileanu@gmail.com>
2017-02-03 14:07:52 +0200

    Checkout part 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { HttpClient } from '../../base';
 import { NavController } from 'ionic-angular';
 import { ProtestService } from '../../providers';
-import { Geolocation, Device } from 'ionic-native';
-import { UUID } from 'angular2-uuid';
-import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'page-home',
@@ -19,21 +16,6 @@ export class HomePage {
   }
 
   checkIn(){
-    if (!localStorage['uuid'])
-      localStorage['uuid'] = Device.uuid ? Device.uuid : UUID.UUID();
-    let uuid = localStorage['uuid'];
-    uuid = btoa(CryptoJS.AES.encrypt(uuid, 'NOWEIMFElmmWJKnwKEJFWNwkjEWNPOF').ciphertext.toString());
-    Geolocation.getCurrentPosition().then((resp) => {
-      var data = {
-        lat: resp.coords.latitude,
-        lon: resp.coords.longitude,
-        prec: resp.coords.accuracy,
-        shout: this.shout,
-        ts: new Date().getTime()/1000,
-        uid: uuid
-      };
-
-      this.protestService.checkIn(data);
-    });
+    this.protestService.checkInNow(this.shout);
   }
 }
--- src/providers/protest.service.ts
+++ src/providers/protest.service.ts
@@ -1,76 +1,101 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable, Inject, NgZone } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import { CONFIG_TOKEN, IConfig } from '../providers/config';
 import { LoggerService } from './logger.service';
 import { HttpClient } from '../base';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
+import { Geolocation, Device } from 'ionic-native';
+import { UUID } from 'angular2-uuid';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable()
 export class ProtestService {
 
-  public currentProtest:any = { id: 1 };
+  public currentProtest: any = { id: 1 };
   private interval: number;
+  private refreshStatusInterval: number;
   private hasHttpError: boolean = false;
 
-  constructor(private http: HttpClient, @Inject(CONFIG_TOKEN) private config: IConfig) {
+  constructor(private http: HttpClient, @Inject(CONFIG_TOKEN) private config: IConfig, private zone: NgZone) {
   }
 
-  checkIn(data:any)
-  { 
-    console.log(this.hasHttpError);
+  checkInNow(shout: string) {
+    if (!localStorage['uuid'])
+      localStorage['uuid'] = Device.uuid ? Device.uuid : UUID.UUID();
+    let uuid = localStorage['uuid'];
+    uuid = btoa(CryptoJS.AES.encrypt(uuid, 'NOWEIMFElmmWJKnwKEJFWNwkjEWNPOF').ciphertext.toString());
+    Geolocation.getCurrentPosition().then((resp) => {
+      var data = {
+        lat: resp.coords.latitude,
+        lon: resp.coords.longitude,
+        prec: resp.coords.accuracy,
+        shout: shout,
+        ts: new Date().getTime() / 1000,
+        uid: uuid
+      };
+
+      this.checkIn(data);
+    });
+  }
+  checkIn(data: any) {
     if (this.hasHttpError)
       return;
     var obs = this.http.post('/', data);
-    console.log('posting...');
-    
+
     obs
-    .timeout(2000)
-    .map(res=>res.json()).subscribe(res => {
-      this.currentProtest = res;
-    },
+      .timeout(2000)
+      .map(res => res.json()).subscribe(res => {
+        this.currentProtest = res;
+        if (!this.refreshStatusInterval) {
+          this.refreshStatusInterval = setInterval(()=>this.refreshCurrentEvent(), this.config.refreshStatusInterval*1000);
+      }},
       err => {
         this.handleError(data)
       }
-    )
-    
+      )
+
     return obs;
   }
-
-  private handleError(data)
-  {
-    this.hasHttpError=true;
-      if(localStorage['queue']){
-        let queue: any[] = JSON.parse(localStorage['queue']);
-        if (!queue.find(itm => itm.uuid === data.uuid && itm.ts === data.ts))
-        {
-          queue.push(data);
-          localStorage['queue'] = JSON.stringify(queue);
-          if (!this.interval)
-            this.interval = setInterval(()=> {
-              this.processQueue();
-            }, 3000);
-        }
+  private refreshCurrentEvent() {
+    Geolocation.getCurrentPosition().then(pos => {
+      if (this.distance(pos.coords.latitude, pos.coords.longitude, this.currentProtest.lat, this.currentProtest.lon, 'K')>this.config.autoCheckoutDistance/1000) {
+        this.currentProtest = { id: 1 };
+        clearInterval(this.refreshStatusInterval);
+        this.refreshStatusInterval = undefined;
       }
-      else
-      {
-        localStorage['queue'] = JSON.stringify([data]);
+    });
+  }
+  private handleError(data) {
+    this.hasHttpError = true;
+    if (localStorage['queue']) {
+      let queue: any[] = JSON.parse(localStorage['queue']);
+      if (!queue.find(itm => itm.uuid === data.uuid && itm.ts === data.ts)) {
+        queue.push(data);
+        localStorage['queue'] = JSON.stringify(queue);
         if (!this.interval)
-          this.interval = setInterval(()=> {
+          this.interval = setInterval(() => {
             this.processQueue();
-          }, 3000);
+      }, this.config.offlinePollingInterval*1000);
       }
+    }
+    else {
+      localStorage['queue'] = JSON.stringify([data]);
+      if (!this.interval)
+        this.interval = setInterval(() => {
+          this.processQueue();
+    }, this.config.offlinePollingInterval*1000);
+    }
   }
 
-  public processQueue(){
-      console.log(localStorage['queue']);
-    if (localStorage['queue'])
-    {
+  public processQueue() {
+    console.log(localStorage['queue']);
+    if (localStorage['queue']) {
       this.hasHttpError = false;
       let queue: any[] = JSON.parse(localStorage['queue']);
       //ping the service
       this.checkIn(queue[0]).subscribe(res => {
-        queue.splice(0,1);
-        queue.forEach(data=>{
+        queue.splice(0, 1);
+        queue.forEach(data => {
           this.checkIn(data);
         });
         clearInterval(this.interval);
@@ -79,5 +104,19 @@ export class ProtestService {
 
       });
     }
+  }
+
+  distance(lat1, lon1, lat2, lon2, unit) {
+    var radlat1 = Math.PI * lat1 / 180
+    var radlat2 = Math.PI * lat2 / 180
+    var theta = lon1 - lon2
+    var radtheta = Math.PI * theta / 180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180 / Math.PI
+    dist = dist * 60 * 1.1515
+    if (unit == "K") { dist = dist * 1.609344 }
+    if (unit == "N") { dist = dist * 0.8684 }
+    return dist
   }
 }
